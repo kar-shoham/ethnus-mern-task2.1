b3f6c3a939108d36540a363f02c69a315620540b
Shoham <kar.shoham.25@gmail.com>
2023-06-20 00:18:19 +0530

    lol

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
   return (
     <Box sx={{display: 'flex', justifyContent: 'center'}} width={'100vw'} paddingTop={15}>
     <Stack p={10} gap={2} fontSize={30}>
-        <Link to={'/task-1'}>Task - 1</Link>
-        <Link to={'/task-2'}>Task - 2</Link>
+        <Link to={'/task-1'} replace >Task - 1</Link>
+        <Link to={'/task-2'} >Task - 2</Link>
         <Link to={'/task-3'}>Task - 3</Link>
         <Link to={'/task-4'}>Task - 4</Link>
         <Link to={'/task-5'}>Task - 5</Link>
